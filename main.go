4c8e43bc7059205a7c546b4d8e895660e5d013e9
VeronicaAlexia <Elaina-Alex@proton.me>
2022-10-26 15:27:07 +0800

    ~

--- main.go
+++ main.go
@@ -9,18 +9,18 @@ import (
 	"pixiv-cil/pkg/config"
 	"pixiv-cil/pkg/file"
 	"pixiv-cil/src"
-	pixiv2 "pixiv-cil/src/pixiv"
+	"pixiv-cil/src/pixiv"
 	"pixiv-cil/utils"
 )
 
 func init() {
 	config.VarsConfigInit()
 	if config.Vars.PixivRefreshToken == "" {
-		PixivRefreshToken, err := pixiv2.ChromeDriverLogin()
+		PixivRefreshToken, err := pixiv.ChromeDriverLogin()
 		if err != nil {
 			panic(err)
 		}
-		if token, ok := pixiv2.InitAuth(PixivRefreshToken); ok != nil {
+		if token, ok := pixiv.InitAuth(PixivRefreshToken); ok != nil {
 			fmt.Println("refresh token is invalid,please login again:", ok)
 		} else {
 			config.VarsFile.Vipers.Set("PIXIV_REFRESH_TOKEN", PixivRefreshToken)
@@ -28,15 +28,15 @@ func init() {
 			config.VarsFile.SaveConfig()
 		}
 	}
-	pixiv2.TokenVariable = config.Vars.PixivToken
-	pixiv2.RefreshTokenVariable = config.Vars.PixivRefreshToken
+	pixiv.TokenVariable = config.Vars.PixivToken
+	pixiv.RefreshTokenVariable = config.Vars.PixivRefreshToken
 }
 
 func main() {
 	file.NewFile("imageFile")
 	cli_app := cli.NewApp()
 	cli_app.Name = "image downloader"
-	cli_app.Version = "V.1.0.9"
+	cli_app.Version = config.Vars.VersionName
 	cli_app.Usage = "download image from pixiv "
 	cli_app.Flags = command.CommandLineFlag
 	cli_app.Action = func(c *cli.Context) error {
