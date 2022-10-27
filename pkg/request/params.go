d56df5f7d35014b9658b9dbd9591a780467cbc15
VeronicaAlexia <Elaina-Alex@proton.me>
2022-10-27 21:56:17 +0800

    ~

--- pkg/request/params.go
+++ pkg/request/params.go
@@ -9,6 +9,15 @@ func (req *Request) QueryData() io.ReadCloser {
 	return io.NopCloser(strings.NewReader(req.EncodeParams()))
 }
 
+func (req *Request) AddParams(key, value string) *Request {
+	req.Params.Add(key, value)
+	return req
+}
+
+func (req *Request) GetParams(key string) string {
+	return req.Params.Get(key)
+}
+
 func (req *Request) EncodeParams(datas ...map[string]string) string {
 	for _, data := range datas {
 		for key, value := range data {
